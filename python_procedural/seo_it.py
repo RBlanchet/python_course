89e60e305f305584863e3f859280631880b14222
David Zarebski <zarebskidavid@gmail.com>
2019-01-28 09:08:31 +0100

    Add files via upload
    initialisation

---
+++ python_procedural/seo_it.py
@@ -0,0 +1,29 @@
+
+
+
+
+# Get the n top results from a google request. Returns a list of url
+
+# PAGE SCALE FUNCTIONS 
+
+## parse a web page. Returns it as an html object or error (identify language)
+
+## extract entity types: returns it as a Json
+
+## get al links of the page: returns it as a (list?)
+
+## get top 20 bigrams: returns it as a list (tweak nltk to use the relavant langage dependencies)
+
+# DOMAIN SCALE FUNCTIONS
+
+# look for robot.txt, follow internal links (by adding them on a stack) , parse all this mess
+# stop when there is no more internal urls and return the results as a json
+
+
+# apply this to all n top resuslts
+def main(request): 
+	pass
+
+
+if __name__ == '__main__':
+	main()
